1363e4218d794a76b618eac54809af0cad542e94
faan <faan@dtu.dk>
2020-11-06 18:37:22 +0100

    Add multiple views

--- app.py
+++ app.py
@@ -7,4 +7,15 @@ app = Flask(__name__)
 def home(q=''):
   return render_template('index.html', q=q)
 
+
+@app.route('/author/<q>')
+def show_author(q='Q20980928'):
+  return render_template('author.html', q=q)
+
+
+@app.route('/topic/<q>')
+def show_topic(q='Q2013'):
+  return render_template('topic.html', q=q)
+
+
 app.run()
